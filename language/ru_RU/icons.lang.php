cf2b6671564316265eb14466ff858d57055f5fcb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-25 10:44:34 +0000

    [easy_captcha] Update ru_RU, thanks to : mistic100 & Konve
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24237 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['magnifying glass'] = 'увеличительное стекло';
+$lang['mobile phone'] = 'мобильник';
+$lang['pencil'] = 'карандаш';
+$lang['printer'] = 'принтер';
+$lang['scissors'] = 'ножницы';
+$lang['shoe'] = 'ботинок';
+$lang['shopping cart'] = 'корзина из магазина';
+$lang['suitcase'] = 'чемодан';
+$lang['trash'] = 'мусор';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = 'мяч';
+$lang['battery'] = 'батарейка';
+$lang['bicycle'] = 'велосипед';
+$lang['binoculars'] = 'бинокль';
+$lang['book'] = 'книга';
+$lang['calculator'] = 'калькулятор';
+$lang['calendar'] = 'календарь';
+$lang['camera'] = 'фотоаппарат';
+$lang['car'] = 'машина';
+$lang['cookie'] = 'печеньки';
+$lang['disk'] = 'диск';
+$lang['guitar'] = 'гитара';
+$lang['hat'] = 'шляпа';
+$lang['house'] = 'дом';
+$lang['lock'] = 'замок';
+?>
