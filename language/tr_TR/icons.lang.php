8d4c24d07b45ac36c7cb7540a750724283664456
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-25 17:11:24 +0000

    [easy_captcha] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24244 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['scissors'] = 'makas';
+$lang['shoe'] = 'ayakkabı';
+$lang['shopping cart'] = 'alışveriş sepeti';
+$lang['suitcase'] = 'bavul';
+$lang['trash'] = 'çöp';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = 'top';
+$lang['battery'] = 'batarya';
+$lang['bicycle'] = 'bisiklet';
+$lang['binoculars'] = 'dürbün';
+$lang['book'] = 'kitap';
+$lang['calculator'] = 'hesap makinesi';
+$lang['calendar'] = 'takvim';
+$lang['camera'] = 'kamera';
+$lang['car'] = 'araba';
+$lang['cookie'] = 'kurabiye';
+$lang['disk'] = 'disk';
+$lang['guitar'] = 'gitar';
+$lang['hat'] = 'şapka';
+$lang['house'] = 'ev';
+$lang['lock'] = 'kilit';
+$lang['magnifying glass'] = 'büyüteç';
+$lang['mobile phone'] = 'taşınabilir telefon';
+$lang['pencil'] = 'kalem';
+$lang['printer'] = 'yazıcı';
+?>
