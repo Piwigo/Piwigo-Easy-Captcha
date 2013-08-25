c7d2013c82866e2709c5ced7de863130ba03736b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-25 10:44:32 +0000

    [easy_captcha] Update da_DK, thanks to : mistic100 & Kaare
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24236 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['scissors'] = 'saks';
+$lang['shoe'] = 'sko';
+$lang['shopping cart'] = 'indkøbsvogn';
+$lang['suitcase'] = 'kuffert';
+$lang['trash'] = 'affald';
+$lang['DVD'] = 'Dvd';
+$lang['ball'] = 'bold';
+$lang['battery'] = 'batteri';
+$lang['bicycle'] = 'cykel';
+$lang['binoculars'] = 'kikkert';
+$lang['book'] = 'bog';
+$lang['calculator'] = 'regnemaskine';
+$lang['calendar'] = 'kalender';
+$lang['camera'] = 'kamera';
+$lang['car'] = 'bil';
+$lang['cookie'] = 'småkage';
+$lang['disk'] = 'disk';
+$lang['guitar'] = 'guitar';
+$lang['hat'] = 'hat';
+$lang['house'] = 'hus';
+$lang['lock'] = 'lås';
+$lang['magnifying glass'] = 'forstørrelsesglas';
+$lang['mobile phone'] = 'mobiltelefon';
+$lang['pencil'] = 'blyant';
+$lang['printer'] = 'printer';
+?>
