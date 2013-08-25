7d8a92260eef00a0386d0a88df315cf7b1d0ce2d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-25 10:44:37 +0000

    [easy_captcha] Update de_DE, thanks to : mistic100 & fs101299
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24240 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -8,4 +8,32 @@ $lang['Contact form'] = 'Kontakt Formular';
 $lang['Guestbook'] = 'Gästebuch';
 $lang['Picture comments'] = 'Bilder Kommentare';
 $lang['Register form'] = 'Registrierungs-Formular ';
+$lang['To verify you are a human, please place the <b>%s</b> in the most right box bellow.'] = 'Um festzustellen, dass du ein Mensch bist, bitte das <b>%s</b> in das äusserste rechte Kästchen platzieren.';
+$lang['You are player X, click on the right case to complete the line.'] = 'Du bist Spieler X, klicke auf den rechten Kasten , um die Zeile zu vervollständigen.';
+$lang['You must activate JavaScript in your browser in order to be able to add a comment, sorry for the inconvenience.'] = 'JavaScript muss aktiviert sein, um einen Kommentar schreiben zu können.
+Entschuldigung für die Unannehmlichkeit.';
+$lang['This challenge requires that JavaScript is enabled on the visitor browser. About 1% of Internet users have Javascript disabled.'] = 'Es wird erwartet, dass JavaScript im Browser aktiviert ist.
+Nur 1% aller Internetnutzer haben JavaScript deaktiviert.';
+$lang['We detected that Crypto Captcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = 'Es wurde festgestellt, dass das Crypto Captcha Plugin in deiner Gallerie verfügbar ist. Beide Plugins können zeitgleich benutzt werden, aber auf keinen Fall dürfen beide auf der gleichen Seite aktiviert sein.';
+$lang['Tic-tac-toe options'] = 'Tic-Tac-Toe Optionen';
+$lang['Background'] = 'Hintergrund';
+$lang['Border'] = 'Rand';
+$lang['Challenge'] = 'Herausforderung';
+$lang['Colors'] = 'Farben';
+$lang['Drag & drop options'] = 'Drag & Drop Optionen';
+$lang['Drop'] = 'Fallen lassen (drop)';
+$lang['Drop border'] = 'drop Rand';
+$lang['Image'] = 'Bild';
+$lang['Image border'] = 'Bildrand';
+$lang['Image drag & drop'] = 'Bild drag & drop';
+$lang['Image size'] = 'Bildgröße';
+$lang['Invalid answer'] = 'ungültige Antwort';
+$lang['Invalid color code <i>%s</i>'] = 'ungültiger Farbcode <i>%s</i>';
+$lang['Marks'] = 'Markierungen';
+$lang['Number of images'] = 'Bilderanzahl';
+$lang['Pleaser answer'] = 'Bitte antworten';
+$lang['Random'] = 'Zufall';
+$lang['Text'] = 'Text';
+$lang['Theme'] = 'Thema';
+$lang['Tic-tac-toe minigame'] = 'Tic-Tyc-Toe Minispiel';
 ?>
