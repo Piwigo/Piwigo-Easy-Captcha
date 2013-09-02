3430e4526e4600fc86b6ac9b313e31a7ede3ce6b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-02 04:30:30 +0000

    [easy_captcha] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24314 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/pets.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['mouse'] = '老鼠';
+$lang['toucan'] = '巨嘴鳥';
+$lang['bat'] = '蝙蝠';
+$lang['butterfly'] = '蝴蝶';
+$lang['cat'] = '貓';
+$lang['dog'] = '狗';
+$lang['duck'] = '鴨';
+$lang['eagle'] = '鷹';
+$lang['elephant'] = '象';
+$lang['fish'] = '魚';
+$lang['fox'] = '狐狸';
+$lang['frog'] = '青蛙';
+$lang['horse'] = '馬';
+$lang['ladybug'] = '瓢蟲';
+$lang['lion'] = '獅子';
+$lang['panda'] = '熊貓';
+$lang['penguin'] = '企鵝';
+$lang['snake'] = '蛇';
+?>
