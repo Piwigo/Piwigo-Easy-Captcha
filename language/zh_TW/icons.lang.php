b68f05c46047e5c402ec371a08110f8d062fdc39
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-02 04:27:28 +0000

    [easy_captcha] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24310 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['scissors'] = '剪刀';
+$lang['shoe'] = '鞋';
+$lang['shopping cart'] = '購物車';
+$lang['suitcase'] = '手提箱';
+$lang['trash'] = '垃圾';
+$lang['disk'] = '唱片';
+$lang['calculator'] = '計算機';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = '球';
+$lang['battery'] = '電池';
+$lang['bicycle'] = '自行車';
+$lang['binoculars'] = '雙筒望遠鏡';
+$lang['book'] = '書';
+$lang['calendar'] = '日曆';
+$lang['camera'] = '相機';
+$lang['car'] = '汽車';
+$lang['cookie'] = '餅乾';
+$lang['guitar'] = '吉他';
+$lang['hat'] = '帽子';
+$lang['house'] = '房子';
+$lang['lock'] = '鎖';
+$lang['magnifying glass'] = '放大鏡';
+$lang['mobile phone'] = '行動電話';
+$lang['pencil'] = '鉛筆';
+$lang['printer'] = '印表機';
+?>
