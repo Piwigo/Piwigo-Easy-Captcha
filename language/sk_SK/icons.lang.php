d267fbb92124b0b8c9f93466f256338c806305b1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-02 18:50:55 +0000

    [easy_captcha] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24322 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['magnifying glass'] = 'lupa';
+$lang['scissors'] = 'nožnice';
+$lang['shoe'] = 'topánka';
+$lang['shopping cart'] = 'nákupný vozík';
+$lang['suitcase'] = 'kufor';
+$lang['trash'] = 'kôš';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = 'lopta';
+$lang['battery'] = 'baterka';
+$lang['bicycle'] = 'bicykel';
+$lang['binoculars'] = 'ďalekohľad';
+$lang['book'] = 'kniha';
+$lang['calculator'] = 'kalkulačka';
+$lang['calendar'] = 'kalendár';
+$lang['camera'] = 'kamera';
+$lang['car'] = 'auto';
+$lang['cookie'] = 'koláč';
+$lang['disk'] = 'disk';
+$lang['guitar'] = 'gitara';
+$lang['hat'] = 'klobúk';
+$lang['house'] = 'dom';
+$lang['lock'] = 'zámok';
+$lang['mobile phone'] = 'mobilný telefón';
+$lang['pencil'] = 'ceruzka';
+$lang['printer'] = 'tlačiareň';
+?>
