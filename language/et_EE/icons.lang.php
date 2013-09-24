095a92864c6d7acd3f85b41f0d39de14e7bc8035
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-24 06:10:58 +0000

    [easy_captcha] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24608 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['suitcase'] = 'kohver';
+$lang['scissors'] = 'käärid';
+$lang['shoe'] = 'king';
+$lang['shopping cart'] = 'ostukäru';
+$lang['trash'] = 'prügi';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = 'pall';
+$lang['battery'] = 'patarei';
+$lang['bicycle'] = 'ratas';
+$lang['binoculars'] = 'binokkel';
+$lang['book'] = 'raamat';
+$lang['calculator'] = 'arvuti';
+$lang['calendar'] = 'kalender';
+$lang['camera'] = 'kaamera';
+$lang['car'] = 'auto';
+$lang['cookie'] = 'küpsis';
+$lang['disk'] = 'diskett';
+$lang['guitar'] = 'kitarr';
+$lang['hat'] = 'müts';
+$lang['house'] = 'maja';
+$lang['lock'] = 'lukk';
+$lang['magnifying glass'] = 'suurendusklaas';
+$lang['mobile phone'] = 'mobiiltelefon';
+$lang['pencil'] = 'pliiats';
+$lang['printer'] = 'printer';
+?>
---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['We detected that Crypto Captcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = ' Crypto Captcha plugin on juba galeriis olemas, mõlemat pluginat saab koos kasutada, aga neid mõlemaid ei tohi koos aktiveerida samal lehel.';
+$lang['This challenge requires that JavaScript is enabled on the visitor browser. About 1% of Internet users have Javascript disabled.'] = 'JavaScript peab olema selle ülesande jaoks aktiveeritud.';
+$lang['You must activate JavaScript in your browser in order to be able to add a comment, sorry for the inconvenience.'] = 'JavaScript peab olema lehitsejas aktiveeritud, et lisada kommentaare';
+$lang['Theme'] = 'Teema';
+$lang['Tic-tac-toe minigame'] = 'Trips-traps-trull minimäng';
+$lang['Tic-tac-toe options'] = 'Trips-traps-trull valikud';
+$lang['To verify you are a human, please place the <b>%s</b> in the most right box bellow.'] = 'Kinnitmakas, et sa oled inimene paiguta <b>%s</b> kõige parempoolsemasse kasti.';
+$lang['You are player X, click on the right case to complete the line.'] = 'Sina oled mängija X, vajuta paremale et lõpetada joon';
+$lang['Drag & drop options'] = 'Lohista ja aseta võimalused';
+$lang['Drop'] = 'Aseta';
+$lang['Drop border'] = 'Aseta raam';
+$lang['Guestbook'] = 'Külalisteraamat';
+$lang['Image'] = 'Pilt';
+$lang['Image border'] = 'Pildi raamid';
+$lang['Image drag & drop'] = 'Pildi lohista ja aseta';
+$lang['Image size'] = 'Pildi suurus';
+$lang['Invalid answer'] = 'mittesobiv vastus';
+$lang['Invalid color code <i>%s</i>'] = 'mittesobiv värvi kood <i>%s</i>';
+$lang['Marks'] = 'Märked';
+$lang['Moderate'] = 'Modereeri';
+$lang['Number of images'] = 'Piltide arv';
+$lang['Picture comments'] = 'Pildi kommentaarid';
+$lang['Pleaser answer'] = 'Palun vasta';
+$lang['Preview'] = 'Eelvaade';
+$lang['Random'] = 'Juhuslik';
+$lang['Register form'] = 'Registreerimise vorm';
+$lang['Text'] = 'Text';
+$lang['Activate on'] = 'Aktiveeri järgmistele';
+$lang['Album comments'] = 'Albumi kommentaarid';
+$lang['Background'] = 'Tagataust';
+$lang['Border'] = 'Piir';
+$lang['Challenge'] = 'Ülesanne';
+$lang['Colors'] = 'Värvid';
+$lang['Comments action'] = 'Kommentaaride tegevused';
+$lang['Contact form'] = 'Kontakteerimise vorm';
+?>
