b9295a941d1ca2a686c9b78f29e7f20e8fbefc2d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-29 20:01:29 +0000

    [easy_captcha] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24692 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/icons.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['trash'] = 'paperera';
+$lang['ball'] = 'pilota';
+$lang['camera'] = 'càmera';
+$lang['car'] = 'cotxe';
+$lang['cookie'] = 'galleta';
+$lang['disk'] = 'disc';
+$lang['mobile phone'] = 'telèfon mòbil';
+$lang['printer'] = 'impressora';
+$lang['scissors'] = 'estisores';
+$lang['shopping cart'] = 'carret de la compra';
+$lang['shoe'] = 'sabata';
+$lang['suitcase'] = 'maleta';
+$lang['DVD'] = 'DVD';
+$lang['battery'] = 'bateria';
+$lang['bicycle'] = 'bicicleta';
+$lang['book'] = 'llibre';
+$lang['calculator'] = 'calculadora';
+$lang['calendar'] = 'calendari';
+$lang['guitar'] = 'guitarra';
+$lang['hat'] = 'barret';
+$lang['house'] = 'caball';
+$lang['pencil'] = 'llapis';
+?>
