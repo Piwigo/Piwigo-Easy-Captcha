8df3c95656d6b594c0a317275c7b965be2e634bc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-02 19:12:45 +0000

    [easy_captcha] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@24729 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['ball'] = 'Bola';
+$lang['scissors'] = 'Tesoura';
+$lang['shoe'] = 'Sapato';
+$lang['shopping cart'] = 'Carro de compras';
+$lang['suitcase'] = 'Mala';
+$lang['trash'] = 'Lixo';
+$lang['DVD'] = 'DVD';
+$lang['battery'] = 'Bateria';
+$lang['bicycle'] = 'Bicicleta';
+$lang['binoculars'] = 'Binóculos';
+$lang['book'] = 'Livro';
+$lang['calculator'] = 'Calculadora';
+$lang['calendar'] = 'Calendário';
+$lang['camera'] = 'Câmara';
+$lang['car'] = 'Carro';
+$lang['cookie'] = 'Biscoito';
+$lang['disk'] = 'Disco';
+$lang['guitar'] = 'Guitarra';
+$lang['hat'] = 'Chapéu';
+$lang['house'] = 'Casa';
+$lang['lock'] = 'Fechar';
+$lang['magnifying glass'] = 'Lupa';
+$lang['mobile phone'] = 'Telemóvel';
+$lang['pencil'] = 'Lápis';
+$lang['printer'] = 'Impressora';
+?>
---
+++ language/pt_PT/pets.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['bat'] = 'Morcego';
+$lang['ladybug'] = 'Joaninha';
+$lang['lion'] = 'Le#ao';
+$lang['toucan'] = 'Tucano';
+$lang['butterfly'] = 'Borboleta';
+$lang['cat'] = 'Gato';
+$lang['dog'] = 'Cão';
+$lang['duck'] = 'Pato';
+$lang['eagle'] = 'Aguia';
+$lang['elephant'] = 'Elefante';
+$lang['fish'] = 'Peixe';
+$lang['fox'] = 'Raposa';
+$lang['frog'] = 'R#a';
+$lang['horse'] = 'Cavalo';
+$lang['mouse'] = 'Rato';
+$lang['panda'] = 'Panda';
+$lang['penguin'] = 'Pinguim';
+$lang['snake'] = 'Cobra';
+?>
