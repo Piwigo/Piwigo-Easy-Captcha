5fb2aacf5b21c33485d764e7a7585b6de221883d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-01 22:36:09 +0000

    [easy_captcha] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@25293 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['battery'] = 'батарейка';
+$lang['bicycle'] = 'велосипед';
+$lang['binoculars'] = 'бінокль';
+$lang['book'] = 'книга';
+$lang['calculator'] = 'калькулятор';
+$lang['calendar'] = 'календар';
+$lang['shopping cart'] = 'кошик';
+$lang['suitcase'] = 'валіза';
+$lang['trash'] = 'сміття';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = 'м’яч';
+$lang['camera'] = 'камера';
+$lang['cookie'] = 'печиво';
+$lang['disk'] = 'диск';
+$lang['guitar'] = 'гітара';
+$lang['hat'] = 'капелюх';
+$lang['lock'] = 'блокування';
+$lang['magnifying glass'] = 'збільшувальне скло';
+$lang['mobile phone'] = 'мобільний телефон';
+$lang['pencil'] = 'олівець';
+$lang['printer'] = 'принтер';
+$lang['scissors'] = 'ножиці';
+$lang['shoe'] = 'взуття';
+$lang['car'] = 'автомобіль';
+$lang['house'] = 'будинок';
+?>
