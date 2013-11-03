fe03ab4d2dc18b7fa9eef4d56643081c4caf0627
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-03 18:53:09 +0000

    [easy_captcha] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@25319 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['scissors'] = '剪刀';
+$lang['shoe'] = '鞋';
+$lang['shopping cart'] = '购物车';
+$lang['suitcase'] = '手提箱';
+$lang['trash'] = '垃圾';
+$lang['DVD'] = 'DVD';
+$lang['ball'] = '球';
+$lang['battery'] = '电池';
+$lang['bicycle'] = '自行车';
+$lang['binoculars'] = '双筒望远镜';
+$lang['book'] = '书';
+$lang['calculator'] = '计算器';
+$lang['calendar'] = '日历';
+$lang['camera'] = '相机';
+$lang['car'] = '车';
+$lang['cookie'] = '曲奇饼';
+$lang['disk'] = '磁盘';
+$lang['guitar'] = '吉他';
+$lang['hat'] = '帽子';
+$lang['house'] = '屋子';
+$lang['lock'] = '锁';
+$lang['magnifying glass'] = '放大镜';
+$lang['mobile phone'] = '手机';
+$lang['pencil'] = '铅笔';
+$lang['printer'] = '打印机';
+?>
---
+++ language/zh_CN/pets.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['bat'] = '蝙蝠';
+$lang['butterfly'] = '蝴蝶';
+$lang['cat'] = '猫';
+$lang['dog'] = '狗';
+$lang['duck'] = '鸭子';
+$lang['eagle'] = '鹰';
+$lang['elephant'] = '大象';
+$lang['fish'] = '鱼';
+$lang['fox'] = '狐狸';
+$lang['frog'] = '蛙';
+$lang['horse'] = '马';
+$lang['ladybug'] = '瓢虫';
+$lang['lion'] = '狮子';
+$lang['mouse'] = '老鼠';
+$lang['panda'] = '熊猫';
+$lang['penguin'] = '企鹅';
+$lang['snake'] = '蛇';
+$lang['toucan'] = '巨嘴鸟';
+?>
