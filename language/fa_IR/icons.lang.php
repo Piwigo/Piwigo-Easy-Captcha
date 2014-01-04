a65fb063516c8a114ebba94db73ab4becfc50431
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-04 15:17:50 +0000

    [easy_captcha] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@26448 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/icons.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['scissors'] = 'قیچی';
+$lang['shoe'] = 'کشف';
+$lang['shopping cart'] = 'کارت فروشگاهی';
+$lang['suitcase'] = 'چمدان';
+$lang['trash'] = 'اشغال';
+$lang['DVD'] = 'دی وی دی';
+$lang['ball'] = 'توپ';
+$lang['battery'] = 'باطری';
+$lang['bicycle'] = 'دچرخه';
+$lang['binoculars'] = 'دوربین دوچشمی';
+$lang['book'] = 'کتاب';
+$lang['calculator'] = 'ماشین حساب';
+$lang['calendar'] = 'تقویم';
+$lang['camera'] = 'دوربین';
+$lang['car'] = 'ماشین';
+$lang['cookie'] = 'کیک';
+$lang['disk'] = 'دیسک';
+$lang['guitar'] = 'گیتار';
+$lang['hat'] = 'داغ';
+$lang['house'] = 'خانه';
+$lang['lock'] = 'قفل';
+$lang['magnifying glass'] = 'ذربین';
+$lang['mobile phone'] = 'موبایل';
+$lang['pencil'] = 'مداد';
+$lang['printer'] = 'نقاشی';
+?>
---
+++ language/fa_IR/pets.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['bat'] = 'خفاش';
+$lang['butterfly'] = 'پروانه ';
+$lang['cat'] = 'گربه';
+$lang['dog'] = 'سگ';
+$lang['duck'] = 'اردک';
+$lang['eagle'] = 'عقاب';
+$lang['elephant'] = 'فیل';
+$lang['fish'] = 'ماهی';
+$lang['fox'] = 'روباه';
+$lang['frog'] = 'وزق';
+$lang['horse'] = 'اسب';
+$lang['ladybug'] = 'کفش دوزک';
+$lang['lion'] = 'شیر';
+$lang['mouse'] = 'موش';
+$lang['panda'] = 'مرچه خوار';
+$lang['penguin'] = 'پنگون';
+$lang['snake'] = 'مار';
+$lang['toucan'] = 'توکان';
+?>
