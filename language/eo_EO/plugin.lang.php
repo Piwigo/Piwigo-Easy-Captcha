6a90a58d718c97a1932ca0376f1dc4aa69b6ed3e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-11 12:04:21 +0000

    [easy_captcha] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/EasyCaptcha@26612 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Challenge'] = 'Testo';
+$lang['Comments action'] = 'Agoj pri la komentoj';
+$lang['Marks'] = 'Markoj';
+$lang['Preview'] = 'Antaŭrigardo';
+$lang['Register form'] = 'Registradoformularo';
+$lang['To verify you are a human, please place the <b>%s</b> in the most right box bellow.'] = 'Por kontroli, ke vi estas homa, bonvolu lokigi <b>%s</b> en la plej dekstra kadro.';
+$lang['Drag & drop options'] = 'Opcioj pri ŝovmeto';
+$lang['Drop'] = 'Demeti';
+$lang['Drop border'] = 'Bordero de demeto';
+$lang['Guestbook'] = 'Gastlibro';
+$lang['Moderate'] = 'Kontroli';
+$lang['Nowhere'] = 'Nenie';
+$lang['Number of images'] = 'Nombro da bildoj';
+$lang['Picture comments'] = 'Bildaj komentoj';
+$lang['Pleaser answer'] = 'Bonvolu respondi';
+$lang['Random'] = 'Hazarda';
+$lang['Text'] = 'Teksto';
+$lang['Theme'] = 'Temo';
+$lang['Activate on'] = 'Aktivigi sur';
+$lang['Album comments'] = 'Fotaraj komentoj';
+$lang['Background'] = 'Fono';
+$lang['Border'] = 'Bordero';
+$lang['Colors'] = 'Koloroj';
+$lang['Contact form'] = 'Kontakta formularo';
+$lang['Image'] = 'Bildo';
+$lang['Image border'] = 'Bildobordero';
+$lang['Image size'] = 'Bilda grandeco';
+$lang['Invalid answer'] = 'Nevalida respondo';
+$lang['Invalid color code <i>%s</i>'] = 'Nevalida kolorkodo <i>%s</i>';
+?>
