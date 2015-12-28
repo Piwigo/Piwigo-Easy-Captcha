f2e1b81c7bc8fbea9f761071cb81ac5d78b4af0c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-12-28 19:09:55 +0100

    Add bg_BG, thanks to turnokopa

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['We detected that Crypto Captcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = 'Установихме, че Crypto Captcha плъгин е достъпна във вашата галерия. И двете приставки могат да се използват по едно и също време, но вие не трябва при никакви обстоятелства да се активират и двете от тях на същата страница.';
+$lang['You must activate JavaScript in your browser in order to be able to add a comment, sorry for the inconvenience.'] = 'Трябва да активирате JavaScript във вашия браузър, за да бъде в състояние да добавите коментар, Съжаляваме за неудобството.';
+$lang['To verify you are a human, please place the <b>%s</b> in the most right box bellow.'] = 'За да потвърдите, че сте човек, моля, поставяйте <b>%s</b> в най-дясната прозорец.';
+$lang['To verify you are a human, please choose the closest color for each symbol.'] = 'За да потвърдите, че сте човек, моля, изберете най-близкия цвят за всеки символ.';
+$lang['To verify you are a human, please beat the game.'] = 'За да потвърдите, че сте човек, моля победи в играта.';
+$lang['Tic-tac-toe options'] = 'Tic-tac-toe опции';
+$lang['Tic-tac-toe minigame'] = 'Tic-tac-toe миниигра';
+$lang['Theme'] = 'Тема';
+$lang['Text'] = 'Текст';
+$lang['Register form'] = 'Форма за регистрация';
+$lang['Random'] = 'Случайно';
+$lang['Preview'] = 'Преглед';
+$lang['Choose at least one challenge'] = 'Избери най-малко едно предизвикателство';
+$lang['Only for unauthenticated users'] = 'Само за неотиризирани потребители';
+$lang['Pleaser answer'] = 'Pleaser answer';
+$lang['Picture comments'] = 'Коментари на снимки';
+$lang['Number of colors'] = 'Брой цветове';
+$lang['Number of images'] = 'Брой снимки';
+$lang['Nowhere'] = 'Никъде';
+$lang['Marks'] = 'Маркирания';
+$lang['Invalid color code <i>%s</i>'] = 'Невалиден номер цвят <i>%s</i>';
+$lang['Challenge'] = 'Предизвикателства';
+$lang['Contact form'] = 'Форма за контакт';
+$lang['Comments action'] = 'Коментари на действията';
+$lang['Colors selector options'] = 'Опиции за избор на цветове';
+$lang['Colors selection'] = 'Избор на цветове';
+$lang['Drop border'] = 'Drop граница';
+$lang['Drag & drop options'] = 'Drag & drop опции';
+$lang['Image size'] = 'Размер на снимка';
+$lang['Invalid answer'] = 'Невалиден отговор';
+$lang['Main border'] = 'Основна граница';
+$lang['Moderate'] = 'Модериране';
+$lang['Drop'] = 'Пускане';
+$lang['Image drag & drop'] = 'Дърпане и пускане на изображение';
+$lang['Image border'] = 'Граница на изображение';
+$lang['Image'] = 'Изображение';
+$lang['Guestbook'] = 'Книга за гости';
+$lang['Colors'] = 'Цветове';
+$lang['Border'] = 'Граница';
+$lang['Background'] = 'Фонд';
+$lang['Arrow background'] = 'Фонд на стрелка';
+$lang['Arrow'] = 'Стрелка';
+$lang['Album comments'] = 'Коментари на албум';
+$lang['Activate on'] = 'Активиране на';
